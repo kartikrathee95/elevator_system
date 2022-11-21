1c87981c14e3922858071cb337cab56d6dcf263f
kartikrathee95 <krtkrathee@gmail.com>
2022-11-21 17:19:12 +0530

    first commit

---
+++ elevator_controller/elevator.py
@@ -0,0 +1,168 @@
+import time
+
+class Elevator(object):
+    def __init__(self, floors, name=""):
+        """Initialize the state of the elevator"""
+        if name != "":
+            self.name = name
+        else:
+            self.name = "Elevator"
+        self.current_floor = 1
+        self.top_floor = floors
+        self.busy = False  
+        self.door_open = False 
+        self.going_up = True 
+        self.go_to_drop = dict()
+        self.go_to = dict()
+        self.no_of_users = 0
+        self.running_status = False
+        for x in range(floors):
+            self.go_to[x + 1] = False
+            self.go_to_drop[x + 1] = False
+        self.user_id = None
+    def __str__(self):
+        return self.name
+
+    def do(self, seconds, action):
+        """Waits for a period of seconds to simulate an action being carried out"""
+        print("                     ")
+        print("Elevator {} _machinery_action".format(self.name))
+        print("%s (%d seconds)" % (action, seconds))
+        print("                     ")
+        print(self.get_status())
+        print("                     ")
+        time.sleep(seconds)
+
+    def get_status(self):
+        go_to = list()
+        for x in self.go_to.keys():
+            if self.go_to[x]:
+                go_to.append(x)
+        return "%(name)s (floor: %(floor)02d, door: %(door)s, direction: %(direction)s, go to: %(go_to)s)" % {
+            "name": self.name,
+            "floor": self.current_floor,
+            "door": "open" if self.door_open else "closed",
+            "direction": "up" if self.going_up else "down",
+            "go_to": repr(go_to)
+        }
+
+    def open_door(self):
+        if not self.door_open:
+            self.do(2, "Opening door")
+            self.door_open = True
+
+    def close_door(self):
+        if self.door_open:
+            self.do(2, "Closing door")
+            self.door_open = False
+
+    def go_to_floor(self, target_floor,state,usid):
+        """Moves the elevator from its current floor to target_floor"""
+        print("the state is {}".format(state))
+        if self.door_open:  
+            self.close_door()
+        original_floor = self.current_floor
+
+        while (self.current_floor != target_floor):
+
+            # Going up!
+            if (self.current_floor < target_floor):
+
+                self.going_up = True
+                if (self.current_floor == original_floor):
+                    self.do(1, "Starting elevator {} at floor ={} ".format(self.name,self.current_floor))
+
+                self.do(2, "Elevator {} Moving from floor {} to {}".format(self.name,self.current_floor, self.current_floor + 1))
+                self.current_floor += 1
+
+                if (self.current_floor == target_floor):
+                    self.do(3, "Stopping elevator {} at floor = {}".format(self.name,self.current_floor))
+                    if state=="drop":
+
+                        print("Passenger {} is being dropped at floor = {} by elevator = {}".format(usid,target_floor,self.name))
+                    else:
+
+                        print("Passenger {} is picked from at floor = {} by elevator = {}".format(usid,target_floor,self.name))
+                else:
+                    target_floor,usid = self.next_floor_up()
+
+            # Going down!
+            elif (self.current_floor > target_floor):
+
+                self.going_up = False
+                if (self.current_floor == original_floor):
+                    self.do(1, "Starting elevator {} at floor = {}".format(self.name,self.current_floor))
+
+                self.do(2, "Elevator {} Moving from floor {} to {}".format(self.name,self.current_floor, self.current_floor - 1))
+                self.current_floor -= 1
+
+                if (self.current_floor == target_floor):
+                    self.do(3, "Stopping elevator {} at floor = {}".format(self.name,self.current_floor))
+
+                    if state=="drop":
+                        print("                                   ")
+                        print("Elevator {} dropping passenger {} at floor = {}".format(self.name,usid,target_floor))
+                        print("                                   ")
+                        print("Passenger {} is being dropped at floor = {} by elevator = {}".format(usid,target_floor,self.name))
+                    else:
+                        print("                                   ")
+                        print("Elevator {} picking up passenger {} at floor = {}".format(self.name,usid,target_floor))
+                        print("                                   ")
+                        print("Passenger {} is picked from at floor = {} by elevator = {}".format(usid,target_floor,self.name))
+
+                else:
+                    target_floor,usid = self.next_floor_down()
+
+        self.go_to[target_floor] = False
+
+    def next_floor_up(self):
+        for f in range(self.current_floor, self.top_floor + 1):
+            if self.go_to[f] != False:
+                return f,self.go_to[f]
+        return None,None
+
+    def next_floor_down(self):
+        for f in range(self.current_floor - 1, 0, -1):
+            if self.go_to[f] != False:
+                return f,self.go_to[f]
+        return None,None
+
+    def run(self,state = "pick"):
+        if self.running_status==False:
+            self.running_status = True
+            while True:
+           
+                if self.going_up:
+                    next_floor,user = self.next_floor_up()
+                else:
+                    next_floor,user = self.next_floor_down()
+
+            # Check floors in the opposite direction if no floor was received
+                if next_floor is None:
+                    if self.going_up:
+                        next_floor,user = self.next_floor_down()
+                    else:
+                        next_floor,user = self.next_floor_up()
+                
+                if next_floor is None:
+                   
+                    if  state=="pick":
+                        state= "drop"
+                        self.go_to = self.go_to_drop
+                        continue
+                    else:
+                        self.running_status = False
+                        return
+                else:
+                    self.busy = True
+                    self.go_to_floor(next_floor,state,user)
+                    if self.current_floor == self.top_floor:
+                        self.going_up = False
+                    elif self.current_floor == 1:
+                        self.going_up = True
+                    self.open_door()
+                    print("                               ")
+                    self.do(5, "Loading/unloading elevator {} at floor = {}".format(self.name,next_floor))
+                    print("                               ")
+                    self.close_door()
+                
---
+++ elevator_controller/interface.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+
+
+class Interface(object):
+    def __init__(self, controller):
+        self.controller = controller
+        self.floors = controller.floors  # number of floors
+        self.window = tk.Tk()
+        self.window.title("ElevatorSimulator")
+        self.displays = dict()
+        self.displays[0] = dict()
+        self.displays[0]["frame"] = tk.Frame(self.window, highlightbackground="black", highlightthickness=1)
+        self.displays[0]["frame"].grid(row=0, column=0)
+        tk.Label(self.displays[0]["frame"], text="Call Elevator").grid(row=0, column=0, columnspan=3)
+        tk.Label(self.displays[0]["frame"]).grid(row=1, column=0, columnspan=3)  # space holder
+        self.displays[0]["calls"] = dict()
+        for i in range(1, self.floors + 1):
+            self.displays[0]["calls"][i] = dict()
+            self.displays[0]["calls"][i]["up"] = tk.Button(
+                self.displays[0]["frame"],
+                text="\u25B2",
+                relief="groove",
+                command=lambda f=i: self.controller.call_elevator(f, "up")
+            )
+            self.displays[0]["calls"][i]["down"] = tk.Button(
+                self.displays[0]["frame"],
+                text="\u25BC",
+                relief="groove",
+                command=lambda f=i: self.controller.call_elevator(f, "down")
+            )
+            tk.Label(self.displays[0]["frame"], text=i).grid(row=self.floors + 2 - i, column=0)
+            if i != self.floors:
+                self.displays[0]["calls"][i]["up"].grid(row=self.floors + 2 - i, column=1)
+            if i != 1:
+                self.displays[0]["calls"][i]["down"].grid(row=self.floors + 2 - i, column=2)
+        self.update_calls_display(False)
+        for i in range(1, len(self.controller.elevators) + 1):
+            display = dict()
+            display["frame"] = tk.Frame(self.window, highlightbackground="black", highlightthickness=1)
+            display["frame"].grid(row=0, column=i)
+            display["name"] = tk.Label(display["frame"], text=self.controller.elevators[i - 1].name)
+            display["name"].grid(row=0, column=0, columnspan=2)
+            display["floor"] = tk.Label(display["frame"])
+            display["floor"].grid(row=1, column=0)
+            display["dir"] = tk.Label(display["frame"])
+            display["dir"].grid(row=1, column=1)
+            display["go_to"] = dict()
+            for j in range(1, self.floors + 1):
+                display["go_to"][j] = tk.Button(display["frame"], text=j, width=3, relief="groove",
+                                                command=lambda e=i - 1, f=j: self.send_elevator(e, f))
+                display["go_to"][j].grid(row=self.floors + 2 - j, column=0, columnspan=2)
+            self.displays[i] = display
+            self.update_display(i, True)
+
+    def send_elevator(self, elevator, floor):
+        self.controller.elevators[elevator].go_to[floor] = True
+
+    def update_calls_display(self, continuous=True):
+        for i in range(1, self.floors + 1):
+            self.displays[0]["calls"][i]["up"]["bg"] = "gold" if self.controller.called[i]["up"] else "SystemButtonFace"
+            self.displays[0]["calls"][i]["down"]["bg"] = "gold" if self.controller.called[i][
+                "down"] else "SystemButtonFace"
+        self.window.update_idletasks()
+        if continuous:
+            self.window.after(500, self.update_calls_display)
+
+    def update_display(self, i, continuous=True):
+        elevator = self.controller.elevators[i - 1]
+        self.displays[i]["floor"]["text"] = elevator.current_floor
+        self.displays[i]["dir"]["text"] = "\u25B2" if elevator.going_up else "\u25BC"
+        for j in range(1, self.floors + 1):
+            # blue: open; green: current floor; gold: called; otherwise, default color
+            if elevator.current_floor == j:
+                self.displays[i]["go_to"][j]["bg"] = "blue" if elevator.door_open else "green"
+            elif elevator.go_to[j]:
+                self.displays[i]["go_to"][j]["bg"] = "gold"
+            else:
+                self.displays[i]["go_to"][j]["bg"] = "SystemButtonFace"
+        self.window.update_idletasks()
+        if continuous:
+            self.window.after(5, lambda d=i: self.update_display(d))
+
+    def update_displays(self):
+        self.update_calls_display()
+        for i in range(1, len(self.controller.elevators) + 1):
+            self.update_display(i)
+
+    def run(self):
+        self.window.after(0, self.update_displays)
+
